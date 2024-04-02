79a8b5c1f2df1c9a98880a3a330f026175fbe832
Chokun Rojanapron <chokun.swc555@gmail.com>
2024-04-02 17:35:13 +0700

    added sample code
    Signed-off-by: Chokun Rojanapron <chokun.swc555@gmail.com>

---
+++ main.py
@@ -0,0 +1,22 @@
+import novapi
+import time
+from mbuild.encoder_motor import encoder_motor_class
+from mbuild import power_expand_board
+from mbuild import gamepad
+from mbuild.smartservo import smartservo_class
+from mbuild import power_manage_module
+
+FR_ENCODE_M1 = encoder_motor_class("M1", "INDEX1")
+BR_ENCODE_M2 = encoder_motor_class("M2", "INDEX1")
+FL_ENCODE_M3 = encoder_motor_class("M3", "INDEX1")
+BL_ENCODE_M4 = encoder_motor_class("M4", "INDEX1")
+
+#run once
+FR_ENCODE_M1.set_power(0)
+BR_ENCODE_M2.set_power(0)
+FL_ENCODE_M3.set_power(0)
+BL_ENCODE_M4.set_power(0)
+
+while True:
+    # run forever
+    pass
